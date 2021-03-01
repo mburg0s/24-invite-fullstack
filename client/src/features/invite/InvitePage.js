54d90b75f7277ff5978d6d290d61597bbc28689e
mburg0s <burgos.marissa@gmail.com>
2021-03-01 09:22:48 -0500

    upload code

--- client/src/features/invite/InvitePage.js
+++ client/src/features/invite/InvitePage.js
@@ -4,25 +4,16 @@ import {  useDispatch, useSelector } from "react-redux";
 import {
     selectInvite,
     getInvite,
-    selectGoing,
-    selectNotGoing,
 } from "./inviteSlice"
 import InviteDetail from './InviteDetail'
 
 export default function InvitePage() {
 
-    // refactor to use slice
-    // const [person, getPerson] =useState({})
     const person = useSelector(selectInvite)
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getInvite())
 
-        // axios.get('/results')
-        // .then(resp => {
-        //     console.log(resp.data, 'data')
-        //     getPerson(resp.data)
-        // })
     }, [])
 
 
